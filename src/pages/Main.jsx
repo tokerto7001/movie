0da0bd2907e28142e0d6bf141f14f5e1c0966a74
Hasan TOKER <tokerto7001@gmail.com>
2022-02-04 23:34:30 +0300

    movie cards

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 
 const UNFILTERED = 'https://api.themoviedb.org/3/discover/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f'
 
@@ -7,10 +8,13 @@ const FILTERED = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3e6
 
 export default function Main() {
 
-    const [searchTerm, setSearchTerm] = useState('')
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movies, setMovies] = useState([]);
 
     const getMovies = (API) => {
-        axios.get(API).then((res) => console.log(res.data.results)).catch((err) => console.log(err))
+        axios.get(API)
+            .then((res) => setMovies(res.data.results))
+            .catch((err) => console.log(err))
     }
 
     const handleSubmit = (e) => {
@@ -40,7 +44,17 @@ export default function Main() {
                 />
             </form>
             <div className="movie-container">
-
+                {
+                    movies.map((movie) => (
+                        <MovieCard
+                            key={movie.id}
+                            title={movie.title}
+                            poster_path={movie.poster_path}
+                            overview={movie.overview}
+                            vote_average={movie.vote_average}
+                        />
+                    ))
+                }
             </div>
         </React.Fragment>
     )
