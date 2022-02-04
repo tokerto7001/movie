f75d0d762619d7a49873fe52eece832192aed330
Hasan TOKER <tokerto7001@gmail.com>
2022-02-05 00:02:44 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
+import Login from './pages/Login'
+import Register from './pages/Register'
 import './App.css'
 
 export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Main />
-    </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
