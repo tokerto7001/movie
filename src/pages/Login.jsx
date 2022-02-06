4d9af253f3986f152978014dfc138f791cac6fe5
Hasan TOKER <tokerto7001@gmail.com>
2022-02-06 22:55:04 +0300

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    return <div>Login</div>;
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [alertClass, setAlertClass] = useState('alert alert-danger d-none')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email && password) {
+            navigate('/');
+        } else {
+            setAlertClass('alert alert-danger')
+        }
+    }
+    return (
+        <div className='login'>
+            <div>
+                <img src='https://picsum.photos/800/800' alt='photo' />
+            </div>
+            <div className='login-form'>
+                <div className={alertClass} role="alert">
+                    Please fill in the blanks!!!
+                </div>
+                <h1 className='form-title display-3'>Login</h1>
+                <form id='login' onSubmit={handleSubmit}>
+                    <div className='mb-4'>
+                        <label for='email' className='form-label display-4'>
+                            Email
+                        </label>
+                        <input
+                            type='email'
+                            className='form-control'
+                            id='email'
+                            placeholder='Enter your email adress...'
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                        />
+                    </div>
+                    <div className='mt-3'>
+                        <label for='password' className='form-label display-4'>
+                            Password
+                        </label>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='password'
+                            placeholder='Enter your password...'
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                        />
+                    </div>
+                    <input
+                        type='submit'
+                        className='btn btn-primary form-control'
+                        value='Login'
+                    />
+                </form>
+            </div>
+        </div>
+    );
 }
