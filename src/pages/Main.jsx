e86eac4e0c70f35f66339b5221a2c23412f1ddaa
Hasan TOKER <tokerto7001@gmail.com>
2022-02-06 23:27:07 +0300

    .env

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 
-const UNFILTERED = 'https://api.themoviedb.org/3/discover/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f'
+const UNFILTERED = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
 
-const FILTERED = 'https://api.themoviedb.org/3/search/movie?api_key=d6278b3dc3e6f8f8376a89851c3f8c8f&query='
+const FILTERED = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=`
 
 export default function Main() {
 
