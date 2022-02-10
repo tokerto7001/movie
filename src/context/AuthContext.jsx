e46875e569e7ab84820c88ae626b153f939d6187
Hasan TOKER <tokerto7001@gmail.com>
2022-02-10 23:46:23 +0300

    Register and Login Page

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = (props) => {
 
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
+    const [loginError, setLoginError] = useState(false);
+
+    const handleCredentials = (firstName, lastName, email, password) => {
+        setCredentials({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        });
+    }
+
+    const handleLogin = (email, password) => {
+        if (credentials.email == email && credentials.password == password) {
+            setLoginError(false);
+            navigate('/');
+        } else {
+            setLoginError(true);
+        }
+    }
 
 
     return (
         <AuthContext.Provider value={{
-
+            credentials: credentials,
+            handleCredentials: handleCredentials,
+            handleLogin: handleLogin,
+            loginError: loginError
         }}>
             {props.children}
         </AuthContext.Provider>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
 
 export default function Login() {
 
-    const navigate = useNavigate();
+    const { handleLogin, loginError } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [alertClass, setAlertClass] = useState('alert alert-danger d-none')
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (email && password) {
-            navigate('/');
+            handleLogin(email, password)
         } else {
-            setAlertClass('alert alert-danger')
+            // setAlertClass('alert alert-danger')
         }
     }
     return (
@@ -22,11 +22,11 @@ export default function Login() {
                 <img src='https://picsum.photos/800/800' alt='photo' />
             </div>
             <div className='login-form'>
-                <div className={alertClass} role="alert">
-                    Please fill in the blanks!!!
+                <div className={loginError ? 'alert alert-danger' : 'alert alert-danger d-none'} role="alert">
+                    Please check your credentials!!!
                 </div>
                 <h1 className='form-title display-3'>Login</h1>
-                <form id='login' onSubmit={handleSubmit}>
+                <form id='login' onSubmit={(e) => handleSubmit(e)}>
                     <div className='mb-4'>
                         <label for='email' className='form-label display-4'>
                             Email
@@ -38,6 +38,7 @@ export default function Login() {
                             placeholder='Enter your email adress...'
                             onChange={(e) => setEmail(e.target.value)}
                             value={email}
+                            required
                         />
                     </div>
                     <div className='mt-3'>
@@ -51,6 +52,7 @@ export default function Login() {
                             placeholder='Enter your password...'
                             onChange={(e) => setPassword(e.target.value)}
                             value={password}
+                            required
                         />
                     </div>
                     <input
